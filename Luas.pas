4d7e86566641de8e1847e0180199d7a20b6e7486
gustisasmita73 <81731742+gustisasmita73@users.noreply.github.com>
2021-04-01 14:01:21 +0800

    Add files via upload

---
+++ Luas.pas
@@ -0,0 +1,13 @@
+program LuasPersegi;
+var panjang : integer;
+    lebar   : integer;
+    Luas    : integer;
+    
+
+begin
+  panjang   := 5;
+  lebar     := 2;
+  luas      := panjang * lebar;
+  
+  writeln ('Luas Persegi adalah ', luas);
+end.
